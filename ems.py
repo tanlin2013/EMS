2e10230d3a0704ad1ad0233ed6e655b7ade843e2
Tan Tao-Lin <tanlin2013@gmail.com>
2017-10-20 04:28:49 +0800

    Create ems.py

---
+++ ems.py
@@ -0,0 +1,32 @@
+import EMS
+import matplotlib.pyplot as plt
+
+def Ae(y):
+    Ae = -a*np.log(b*y**2+1)
+    return Ae
+    
+def f(y):
+    f = np.exp(-c*y**2-Ae(y))
+    return f
+
+if __name__=='__main__':
+    
+    a=4.046
+    b=0.01613
+    c=0.227
+    d=3
+    zs=np.linspace(0.,30.,80)
+    zh=10.0
+    mu=1.0
+    
+    gs = []
+    for z in zs:
+        g = EMS.bg.blackening_factor(d,z,zh,mu,f,Ae)
+        gs.append(g)
+        
+    plt.figure()
+    plt.plot(zs,gs,marker='o',linestyle='-')
+    plt.xlim(0,12)
+    plt.ylim(-0.5,1.2)
+    plt.grid()
+    plt.show()
