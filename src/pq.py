426c22222218329b96c691372fa127c2e80dad0f
Tan Tao-Lin <tanlin2013@gmail.com>
2017-10-20 14:21:42 +0800

    Update pq.py

--- src/pq.py
+++ src/pq.py
@@ -11,6 +11,6 @@ def T_BH(g,zh,dz=1e-8):
     T_BH = - derivative(g,zh,dz)/(4*np.pi)
     return T_BH
     
-def sigma():
-    sigma = 
-    return sigma
+#def sigma():
+#    sigma = 
+#    return sigma
