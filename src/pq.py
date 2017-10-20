6c013081bbaf3f9dad83bb472df4d34810f7ae81
phyuan <32847919+phyuan@users.noreply.github.com>
2017-10-20 17:37:44 +0800

    Update pq.py

--- src/pq.py
+++ src/pq.py
@@ -1,3 +1,4 @@
+import bg
 from scipy.misc import derivative
 
 def _partial_derivative(func,axis=0,point=[],n=1,dx=1e-8):
@@ -6,11 +7,32 @@ def _partial_derivative(func,axis=0,point=[],n=1,dx=1e-8):
         args[axis] = x
         return func(*args)
     return derivative(func_along,point[axis],dx,n)
-    
+
+def s_BH(d,Ae,zh,dz=1e-8):
+    s_BH = np.exp(3*bg.we(zh,Ae))/zh**3
+    return s_BH
+
 def T_BH(g,zh,dz=1e-8):
     T_BH = - derivative(g,zh,dz)/(4*np.pi)
     return T_BH
+
+def F_BH(g,zh,dz=1e-8):
+    func = lambda y: s_BH()*
+    F_BH, err = integrate.quad(func,yi,yf)
+    return F_BH
+
+def sigma(z,Ae):
+    sigma = ws(z)**2*np.sqrt(g(z,Ae,dz))
+    return sigma
     
-#def sigma():
-#    sigma = 
-#    return sigma
+    
+def Vqq():
+    func = 
+    Vqq, err = integrate.quad
+    return Vqq
+
+def rqq():
+    func =
+    rqq,err = integrate.quad
+    return rqq
+    
