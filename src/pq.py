e290a27b0298fef3b851b1d17010e118e2bf4e89
Tan Tao-Lin <tanlin2013@gmail.com>
2017-10-21 01:23:24 +0800

    Update pq.py

--- src/pq.py
+++ src/pq.py
@@ -17,8 +17,8 @@ def T_BH(model,zh,mu,dz=1e-8):
     T_BH = - _partial_derivative(bg.blackening_factor,axis=1,point=[model,zh,zh,mu,dz],dx=dz)/(4*np.pi)
     return T_BH
 
-def F_BH(model,zh,mu,dzh=1e-8):
-    func = lambda y: s_BH(model,y)*_partial_derivative(T_BH,axis=1,point=[model,y,mu,dzh],dx=dz)
+def F_BH(model,zh,mu,tol=1e-8):
+    func = lambda y: s_BH(model,y)*_partial_derivative(T_BH,axis=1,point=[model,y,mu,dzh],dx=tol)
     F_BH, err = integrate.quad(func,zh,np.inf)
     return F_BH
 
@@ -26,15 +26,15 @@ def sigma(model,z,zh,mu,tol=1e-8):
     sigma = bg.ws(model,z)**2 * np.sqrt(bg.blackening_factor(model,z,zh,mu,tol))
     return sigma
     
-def Vqq(model,zh,zt,mu):
+def Vqq(model,zh,zt,mu,tol=1e-8):
     func = lambda y: 2/(4*np.pi) * bg.ws(model,y)**2 * (
-        1 - (sigma(model,zt,zh,mu)/sigma(model,y,zh,mu))**2)**(-0.5)
+        1 - (sigma(model,zt,zh,mu,tol)/sigma(model,y,zh,mu,tol))**2)**(-0.5)
     Vqq, err = integrate.quad(func,0,zt)
     return Vqq
 
-def rqq():
-    func = 2*(g(z,zh,dz)*(sigma(z,Ae)^2)/sigma(zt)^2-1)^(-1/2)
-    rqq,err = integrate.quad
+def rqq(model,zh,zt,mu,tol=1e-8):
+    func = lambda y: 2*(bg.blackening_factor(model,z,zh,mu,tol)*((sigma(model,y,zh,mu,tol)/sigma(model,zt,zh,mu,tol))**2-1))**(-0.5)
+    rqq,err = integrate.quad(func,0,zt)
     return rqq
     
 def S_TH():
