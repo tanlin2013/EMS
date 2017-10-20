a84b1f1a20f5fcd3bd1504f94f07b55be796397f
Tan Tao-Lin <tanlin2013@gmail.com>
2017-10-20 14:09:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'EMS',
     packages = ['EMS'],
-    package_dir = {'': 'src'},
+    package_dir = {'EMS': 'src'},
     description = '',
     author = 'tan, phy'
 )
